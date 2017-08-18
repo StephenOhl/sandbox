e6e441652facfd3a9124aaa0abd57f16bc729ad1
StephenOhl <stephen.ohl@my.jcu.edu.au>
2017-08-18 10:30:25 +1000

    Add name input, checking and output

--- oddName.py
+++ oddName.py
@@ -1,3 +1,14 @@
 """
 Stephen Ohl
 """
+
+def main () :
+    name = input ( "Please enter your name : ")
+    while len ( name ) < 1 :
+        print ( "\n\nERROR - Your name can not be nothing\n")
+        name = input("Please enter your name : ")
+
+    for i in range ( 1, len ( name ), 2 ) :
+        print ( name[i] )
+
+main ()
